0eeacd50157b6ea358abd6c2e447ca410654c73c
Medarox02 <96820119+Helmi09@users.noreply.github.com>
2023-05-24 22:43:26 +0100

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import cloudinary_storage
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'base',
-      'cloudinary_storage',
+   
     'cloudinary',
 ]
 
